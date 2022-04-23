384705eae7d41a2c5f945a0f31d1cfea67e2db23
Timuçin Boldt <timucin.boldt@udo.edu>
2022-04-23 18:44:39 +0200

    let character move and slide on left and right

---
+++ Character.gd
@@ -0,0 +1,12 @@
+extends KinematicBody2D
+
+var velocity = Vector2(0, 0)
+var movement_speed = 200
+
+func _physics_process(delta):
+	if Input.is_action_pressed("right"):
+		velocity.x = movement_speed
+	if Input.is_action_pressed("left"):
+		velocity.x = - movement_speed
+	
+	move_and_slide(velocity, Vector2.UP)
