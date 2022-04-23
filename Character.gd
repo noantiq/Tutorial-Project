95257f0d790622e79dfdef5b57083e27e8ae39c6
Timuçin Boldt <timucin.boldt@udo.edu>
2022-04-23 19:01:09 +0200

    let character (double) jump

--- Character.gd
+++ Character.gd
@@ -1,14 +1,26 @@
 extends KinematicBody2D
 
-var velocity = Vector2(0, 0)
-var movement_speed = 200
+var velocity = Vector2.ZERO
+const MOVEMENT_SPEED = 200
+const GRAVITY = 30
+const JUMP_FORCE = - 900
+var jump_counter = 0
+var max_jumps = 2
 
 func _physics_process(delta):
 	if Input.is_action_pressed("right"):
-		velocity.x = movement_speed
+		velocity.x = MOVEMENT_SPEED
 	if Input.is_action_pressed("left"):
-		velocity.x = - movement_speed
+		velocity.x = - MOVEMENT_SPEED
 	
-	move_and_slide(velocity, Vector2.UP)
+	if is_on_floor():
+		jump_counter = 0
 	
-	velocity.x = lerp(velocity.x, 0, 0.15)
+	if Input.is_action_just_pressed("jump") and ((jump_counter < max_jumps) or is_on_floor()):
+		velocity.y = JUMP_FORCE
+		jump_counter += 1
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	velocity.y = velocity.y + GRAVITY
+	velocity.x = lerp(velocity.x, 0, 0.20)
