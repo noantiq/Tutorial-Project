39c373f6596313ac0c7d009cde507691853c9758
Timuçin Boldt <timucin.boldt@udo.edu>
2022-04-24 18:26:59 +0200

    add running and rearange Character code

--- Character.gd
+++ Character.gd
@@ -1,40 +1,72 @@
 extends KinematicBody2D
 
 enum States {AIR, FLOOR, LADDER, WALL}
+var state = States.AIR
 var velocity = Vector2.ZERO
 const MOVEMENT_SPEED = 200
+const RUN_SPEED_MODIFIER = 2
 const GRAVITY = 30
 const JUMP_FORCE = - 900
 var jump_counter = 0
 var max_jumps = 2
 
 func _physics_process(delta):
-	if Input.is_action_pressed("right"):
-		velocity.x = MOVEMENT_SPEED
-		$Sprite.play("walking")
-		$Sprite.flip_h = false
-	elif Input.is_action_pressed("left"):
-		velocity.x = - MOVEMENT_SPEED
-		$Sprite.play("walking")
-		$Sprite.flip_h = true
-	else:
-		$Sprite.play("idle")
 	
-	if is_on_floor():
-		jump_counter = 0
-	else:
-		$Sprite.play("jumping")
-	
-	if Input.is_action_just_pressed("jump") and ((jump_counter < max_jumps) or is_on_floor()):
-		velocity.y = JUMP_FORCE
-		$JumpSound.play()
-		jump_counter += 1
-	
-	velocity = move_and_slide(velocity, Vector2.UP)
-	
-	velocity.y = velocity.y + GRAVITY
-	velocity.x = lerp(velocity.x, 0, 0.20)
+	match state:
+		States.AIR:
+			if is_on_floor():
+				state = States.FLOOR
+				continue
+			$Sprite.play("air")
+			var speed_modifier
+			if Input.is_action_pressed("left"):
+				speed_modifier = - 1
+				$Sprite.flip_h = true
+			elif Input.is_action_pressed("right"):
+				speed_modifier = 1
+				$Sprite.flip_h = false
+			else:
+				speed_modifier = 0
 
+			if Input.is_action_just_pressed("jump") and ((jump_counter < max_jumps)):
+				velocity.y = JUMP_FORCE
+				$JumpSound.play()
+				jump_counter += 1
+			move_and_fall(speed_modifier)
+			
+		States.FLOOR:
+			if not is_on_floor():
+				state = States.AIR
+			jump_counter = 0
+			var speed_modifier
+			if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
+				$Sprite.play("walking")
+				$Sprite.set_speed_scale(1)
+				speed_modifier = - 1 if Input.is_action_pressed("left") else 1
+				$Sprite.flip_h = true if Input.is_action_pressed("left") else false
+				
+				if Input.is_action_pressed("run"):
+					speed_modifier = speed_modifier * RUN_SPEED_MODIFIER
+					$Sprite.set_speed_scale(2)
+			else:
+				$Sprite.play("idle")
+				speed_modifier = 0
+			
+			if Input.is_action_just_pressed("jump"):
+				velocity.y = JUMP_FORCE
+				$JumpSound.play()
+				jump_counter += 1
+				state = States.AIR
+			move_and_fall(speed_modifier)
+		States.LADDER:
+			pass
+		States.WALL:
+			pass
+
+func move_and_fall(speed_modifier):
+	velocity.x = lerp(velocity.x, MOVEMENT_SPEED * speed_modifier, 0.1)
+	velocity = move_and_slide(velocity, Vector2.UP)
+	velocity.y = velocity.y + GRAVITY
 
 func _on_fallzone_body_entered(body):
 	get_tree().change_scene("res://GameOver.tscn")
--- enemy.gd
+++ enemy.gd
@@ -4,7 +4,7 @@ var velocity = Vector2.ZERO
 const GRAVITY = 20
 var movement_speed = 50
 enum DIRECTION {RIGHT = 1, LEFT = - 1}
-export var facing_direction = DIRECTION.LEFT
+export var direction = DIRECTION.LEFT
 export var can_detect_cliffs = true
 
 func _ready():
@@ -14,20 +14,20 @@ func _ready():
 		set_modulate(Color(1.2, 0.5, 1))
 
 func set_direction():
-	$floor_detection.position.x = $CollisionShape2D.shape.get_extents().x * facing_direction
-	if facing_direction == DIRECTION.RIGHT:
+	$floor_detection.position.x = $CollisionShape2D.shape.get_extents().x * direction
+	if direction == DIRECTION.RIGHT:
 		$Sprite.flip_h = true
 	else:
 		$Sprite.flip_h = false
 
 func _physics_process(delta):
 	if is_on_wall() or can_detect_cliffs and not $floor_detection.is_colliding() and is_on_floor():
-		facing_direction = facing_direction * - 1
+		direction = direction * - 1
 		set_direction()
 	
 	velocity.y += GRAVITY
 	
-	velocity.x = int(facing_direction) * movement_speed
+	velocity.x = int(direction) * movement_speed
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
 
